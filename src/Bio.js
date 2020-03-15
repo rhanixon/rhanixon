9b76180a4c8b624fc979982824d74db55c777ae3
Bobby Nixon <rhanixon1@gmail.com>
2020-03-15 10:17:32 -0700

    added draft bio

--- src/Bio.js
+++ src/Bio.js
@@ -9,7 +9,7 @@ export default class Bio extends Component {
           </div>
           <div className="boxed">
           <div className="bio-item">
-            <p className="oneandquarter">I am a fullstack software engineer based in Manhatten. I have a background in math & came to programming from quantitative fianance. I am a passionate problem sovler with an apreciation for design. When I'm not programming I love to spend my time on the ocean whether it be surfing, or sailing. </p>
+            <p className="oneandquarter">I am a full stack software engineer based in Manhattan. I have a background in math & came to programming from quantitative finance. I am a passionate problem solver with an appreciation for design. When I'm not programming I love to spend my time on the ocean whether it be surfing, or sailing.</p>
           </div>
         </div>
       </div>
