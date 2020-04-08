4215f10bb0dfd1f46b2510c217614da0ddc29648
Bobby Nixon <rhanixon1@gmail.com>
2020-04-08 10:47:16 -0700

    changed bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,13 +10,12 @@ export default class Bio extends Component {
         <div className="boxed">
           <div className="bio-item">
             <p className="oneandquarter">
-              I am a full stack software engineer based in Manhattan. I have a
-              background in math and came to programming from quantitative
-              finance. I am a passionate problem solver with an appreciation for
-              design. When I'm not programming I like to spend my time on the
-              ocean, wether it be surfing, sailing, or scuba diving. You can
-              check out some of my projects and writing by clicking my name or
-              the arrow below.
+              I'm a 23 year old full stack software engineer based in Manhattan.
+              I have a background in math and came to programming from
+              quantitative finance. When I'm not programming I like to spend my
+              time on the ocean, wether it be surfing, sailing, or scuba diving.
+              You can check out some of my projects and writing by clicking my
+              name or the arrow below.
             </p>
           </div>
         </div>
