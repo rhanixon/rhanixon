29c1935856f8b270f26a9a6011333f10b76dc1db
Bobby Nixon <rhanixon1@gmail.com>
2020-04-23 14:31:05 -0400

    added fullstack graduation to bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,10 +12,11 @@ export default class Bio extends Component {
             <p className="oneandquarter">
               I'm a 23 year old full stack software engineer based in Manhattan.
               I have a background in math and came to programming from
-              quantitative finance. When I'm not programming I like to spend my
-              time on the ocean, wether it be surfing, sailing, or scuba diving.
-              You can check out some of my projects and writing by clicking my
-              name or the arrow below.
+              quantitative finance. In March of 2020 I completed the Fullstack
+              Academy bootcamp program. When I'm not programming I like to spend
+              my time on the ocean, wether it be surfing, sailing, or scuba
+              diving. You can check out some of my projects and writing by
+              clicking my name or the arrow below.
             </p>
           </div>
         </div>
