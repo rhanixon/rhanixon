36ed0ee9a1a996ef35adebf7322e90eafb2e0828
Bobby Nixon <57637894+rhanixon@users.noreply.github.com>
2020-07-22 20:08:44 -0400

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,12 +10,9 @@ export default class Bio extends Component {
         <div className="boxed">
           <div className="bio-item">
             <p className="oneandquarter">
-              I'm a 23 year old full stack software engineer based in Manhattan.
+              Bootcamp grad and software engineer at Bloomberg LP in New York.
               I have a background in math and came to programming from
-              quantitative finance. In March of 2020 I completed the Fullstack
-              Academy bootcamp program. When I'm not programming I like to spend
-              my time on the ocean, whether it be surfing, sailing, or scuba
-              diving. You can check out some of my projects and writing by
+              quantitative finance. You can check out some of my projects and writing by
               clicking my name or the arrow below.
             </p>
           </div>
